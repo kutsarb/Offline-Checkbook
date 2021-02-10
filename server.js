c8b6c7afbef9fb6f50825b772ac4cba5a36feaf1
1 <kutsarb@gmail.com>
2021-02-09 20:15:41 -0500

    changed port

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 const app = express();
 
 app.use(logger("dev"));
